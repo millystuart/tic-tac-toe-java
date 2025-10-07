e8f41b768789cbebc9f5fc4cfa9e605548a670da
millystuart <melissaaa.stuart@gmail.com>
2025-10-07 12:36:44 +0100

    Create ArrayList to track available positions on the grid

--- org/example/ticTacToe.java
+++ org/example/ticTacToe.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ticTacToe {
     
@@ -30,6 +31,13 @@ public class ticTacToe {
         // Set win tracking boolean to false for initialisation
         boolean hasWon = false;
         
+        // Define ArrayList to hold the open moves on the grid (all are available by default
+        ArrayList<Integer> availableMoves = new ArrayList<Integer>();
+        // Populate
+        for (int i = 1; i < 10; i++) {
+            availableMoves.add(i);
+        }
+        
         // Initialise isPlayerTurn to True for first turn
         boolean isPlayerTurn = true;
                 
@@ -37,15 +45,19 @@ public class ticTacToe {
         
         while (hasWon == false) {
             
-            int playerMove = getPlayerMove(scanner);
-            
-            updateGridWithMove(grid, playerMove, playerSymbol);
-            
-            outputGrid(grid);
+            if (isPlayerTurn = true) {
+                int playerMove = getPlayerMove(scanner, availableMoves);
+                
+                updateGridWithMove(grid, playerMove, playerSymbol); 
+                
+                outputGrid(grid);
+                
+                isPlayerTurn = false;
+            }            
         }
     }
 
-    private static int getPlayerMove(Scanner scanner) {
+    private static int getPlayerMove(Scanner scanner, ArrayList<Integer> availableMoves) {
         
         // Variable accessible across method to hold user's validated move (initialised to dummy value -1)
         int move = -1;
@@ -56,7 +68,7 @@ public class ticTacToe {
         // Output gridIndexes if user types HELP:
         if (userInput.equals("HELP")) {
             outputGrid(GRID_INDEXES);
-            return getPlayerMove(scanner);
+            return getPlayerMove(scanner, availableMoves);
         }
         else {
             
@@ -64,47 +76,47 @@ public class ticTacToe {
                 move = Integer.parseInt(userInput);
             } catch (NumberFormatException e) {
                 System.out.println("Illegal value inputted. Please ensure that your choice is a number.");
-                return getPlayerMove(scanner);
+                return getPlayerMove(scanner, availableMoves);
             }
             
             if (move > 9 || move <= 0 ) {
                 System.out.println("Illegal position on the grid. Please ensure that your choice is between 1 & 9 inclusive");
-                return getPlayerMove(scanner);
-            }
+                return getPlayerMove(scanner, availableMoves);
+            }            
         }
         
         return move;
     }
     
-    private static void updateGridWithMove(char[][] grid, int playerMove, char playerSymbol) {
+    private static void updateGridWithMove(char[][] grid, int move, char symbol) {
         
-        switch (playerMove) {
+        switch (move) {
             case 1:
-                grid[0][0] = playerSymbol;
+                grid[0][0] = symbol;
                 break;
             case 2:
-                grid[0][1] = playerSymbol;
+                grid[0][1] = symbol;
                 break;
             case 3:
-                grid[0][2] = playerSymbol;
+                grid[0][2] = symbol;
                 break;
             case 4:
-                grid[1][0] = playerSymbol;
+                grid[1][0] = symbol;
                 break;
             case 5:
-                grid[1][1] = playerSymbol;
+                grid[1][1] = symbol;
                 break;
             case 6:
-                grid[1][2] = playerSymbol;
+                grid[1][2] = symbol;
                 break;
             case 7:
-                grid[2][0] = playerSymbol;
+                grid[2][0] = symbol;
                 break;
             case 8:
-                grid[2][1] = playerSymbol;
+                grid[2][1] = symbol;
                 break;
             case 9:
-                grid[2][2] = playerSymbol;
+                grid[2][2] = symbol;
                 break;
             default:
                 System.out.println("ERROR: none of the cases were entered, despite validation");
