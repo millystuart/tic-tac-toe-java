745018e5e0a732649d61fdaacb25ba93fc5d1baf
millystuart <melissaaa.stuart@gmail.com>
2025-10-07 12:00:55 +0100

    Ensure that code uses new symbol choice functionality throughout

--- org/example/ticTacToe.java
+++ org/example/ticTacToe.java
@@ -22,13 +22,18 @@ public class ticTacToe {
         
         // Define symbol player will use to play (' ' dummy value as default)
         char playerSymbol = choosePlayerSymbol(scanner);
+        // Give the computer the opposing symbol
+        char computerSymbol = (playerSymbol == 'X') ? 'O' : 'X';
         
-        getPlayerMove(grid, scanner);
+        // Get the player's next move location
+        int playerMove = getPlayerMove(scanner);
+        
+        updateGridWithMove(grid, playerMove, playerSymbol);
         
         outputGrid(grid);
     }
 
-    private static void getPlayerMove(char[][] grid, Scanner scanner) {
+    private static int getPlayerMove(Scanner scanner) {
         
         // Variable accessible across method to hold user's validated move (initialised to dummy value -1)
         int move = -1;
@@ -39,8 +44,7 @@ public class ticTacToe {
         // Output gridIndexes if user types HELP:
         if (userInput.equals("HELP")) {
             outputGrid(GRID_INDEXES);
-            getPlayerMove(grid, scanner);
-            return;
+            return getPlayerMove(scanner);
         }
         else {
             
@@ -48,50 +52,47 @@ public class ticTacToe {
                 move = Integer.parseInt(userInput);
             } catch (NumberFormatException e) {
                 System.out.println("Illegal value inputted. Please ensure that your choice is a number.");
-                getPlayerMove(grid, scanner);
-                return;
+                return getPlayerMove(scanner);
             }
             
             if (move > 9 || move <= 0 ) {
                 System.out.println("Illegal position on the grid. Please ensure that your choice is between 1 & 9 inclusive");
-                getPlayerMove(grid, scanner);
-                return;
+                return getPlayerMove(scanner);
             }
         }
         
-        updateGridWithMove(grid, move);
-        return;
+        return move;
     }
     
-    private static void updateGridWithMove(char[][] grid, int playerMove) {
+    private static void updateGridWithMove(char[][] grid, int playerMove, char playerSymbol) {
         
         switch (playerMove) {
             case 1:
-                grid[0][0] = 'X';
+                grid[0][0] = playerSymbol;
                 break;
             case 2:
-                grid[0][1] = 'X';
+                grid[0][1] = playerSymbol;
                 break;
             case 3:
-                grid[0][2] = 'X';
+                grid[0][2] = playerSymbol;
                 break;
             case 4:
-                grid[1][0] = 'X';
+                grid[1][0] = playerSymbol;
                 break;
             case 5:
-                grid[1][1] = 'X';
+                grid[1][1] = playerSymbol;
                 break;
             case 6:
-                grid[1][2] = 'X';
+                grid[1][2] = playerSymbol;
                 break;
             case 7:
-                grid[2][0] = 'X';
+                grid[2][0] = playerSymbol;
                 break;
             case 8:
-                grid[2][1] = 'X';
+                grid[2][1] = playerSymbol;
                 break;
             case 9:
-                grid[2][2] = 'X';
+                grid[2][2] = playerSymbol;
                 break;
             default:
                 System.out.println("ERROR: none of the cases were entered, despite validation");
