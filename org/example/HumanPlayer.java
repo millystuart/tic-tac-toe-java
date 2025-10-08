f9073e7015c66ca029576d25aaa390359243768c
millystuart <melissaaa.stuart@gmail.com>
2025-10-08 12:44:45 +0100

    Add Javadocs to HumanPlayer class

--- org/example/HumanPlayer.java
+++ org/example/HumanPlayer.java
@@ -3,6 +3,9 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Handles operations that take player input, as well as player-specific operations.
+ */
 public class HumanPlayer implements Player {
     private Grid grid;
     private NodeState symbol;
@@ -17,19 +20,24 @@ public class HumanPlayer implements Player {
         scanner = new Scanner(System.in);        
         symbol = chooseSymbol(scanner);
     }
-
+    
+    /**
+     * Handles the logic behind a player's turn.
+     */
     @Override
     public void makeMove(ArrayList<Integer> availableMoves) {
         int move = getPlayerMove(scanner, availableMoves);
-        
         // Once player move has been chosen and validated, remove the entry from availableMoves
         availableMoves.remove((Object) move);
-        
         grid.updateGridWithMove(move, symbol); 
-        
         grid.displayGrid();
     }
     
+    /**
+     * Allows player to choose a symbol to play as (between X or O).
+     * @param scanner object used to get user input.
+     * @return a NodeState between X or O (their chosen symbol).
+     */
     private NodeState chooseSymbol(Scanner scanner) {
         System.out.println("Would you like to play as a nought (O) or a cross (X)?");
         String userInput = scanner.nextLine().trim().toUpperCase();
@@ -43,10 +51,15 @@ public class HumanPlayer implements Player {
         return desiredSymbol;
     }
     
+    /**
+     * Asks the player where they would like to move, and returns this as an integer location.
+     * @param scanner object used to get user input.
+     * @param availableMoves ArrayList of the remaining moves available to choose from.
+     * @return chosen location (as an integer).
+     */
     private static int getPlayerMove(Scanner scanner, ArrayList<Integer> availableMoves) {
         // Variable accessible across method to hold user's validated move (initialised to dummy value -1)
         int move = -1;
-         
         System.out.println("Where would you like to play your next move? (1-9) \n(type HELP to view grid indexes)");
         String userInput = scanner.nextLine();
         
@@ -55,6 +68,7 @@ public class HumanPlayer implements Player {
             displayHelpGrid();
             return getPlayerMove(scanner, availableMoves);
         }
+        
         else {
             try {
                 move = Integer.parseInt(userInput);
@@ -73,10 +87,12 @@ public class HumanPlayer implements Player {
                 return getPlayerMove(scanner, availableMoves);
             }
         }
-        
         return move;
     }
     
+    /**
+     * Displays the help grid, a grid that shows each node and its corresponding number position.
+     */
     private static void displayHelpGrid() {
         System.out.println(" " + "1" + " | " + "2" + " | " + "3");
         System.out.println("---+---+---");
@@ -85,6 +101,9 @@ public class HumanPlayer implements Player {
         System.out.println(" " + "7" + " | " + "8" + " | " + "9");
     }
     
+    /**
+     * Closes the scanner.
+     */
     public void closeScanner() {
         scanner.close();
     }
