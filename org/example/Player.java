411fbc53a2abadf2377a693e89223456cbbfa5df
millystuart <melissaaa.stuart@gmail.com>
2025-10-08 12:51:52 +0100

    Add Javadoc to Player interface

--- org/example/Player.java
+++ org/example/Player.java
@@ -2,6 +2,9 @@ package org.example;
 
 import java.util.ArrayList;
 
+/**
+ * Dictates that all players must have a way of making a move using the remaining moves available.
+ */
 public interface Player {    
     public void makeMove(ArrayList<Integer> availableMoves);
 }
