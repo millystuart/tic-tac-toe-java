bbc7277d722bd76e7dea4c1e608c931310dca35a
millystuart <melissaaa.stuart@gmail.com>
2025-10-08 12:22:02 +0100

    Add Javadocs to GameController class

--- org/example/GameController.java
+++ org/example/GameController.java
@@ -2,7 +2,11 @@ package org.example;
 
 import java.util.ArrayList;
 
+/**
+ * Handles all of the logic needed to play the game.
+ */
 public class GameController {
+    
     private Grid grid;
     private HumanPlayer player;
     private ComputerPlayer computer;
@@ -18,15 +22,19 @@ public class GameController {
         isWinner = false;
         isPlayerTurn = false;
     }
-
+    
+    /**
+     * Handles the logic behind the game loop, dictating turn order and variable reassignments.
+     * Also runs the end-game sequence once either a winner has been detected or there are no remaining moves.
+     */
     public void gameLoop() {
-                        
         while (!isWinner && !remainingMoves.isEmpty()) {
             if (isPlayerTurn == true) {
                 player.makeMove(remainingMoves);
                 isPlayerTurn = false;
                 isWinner = grid.detectWin(player.getPlayerSymbol());
             }
+            
             else {
                 computer.makeMove(remainingMoves);
                 isPlayerTurn = true;
@@ -35,22 +43,32 @@ public class GameController {
         }
         endGameSequence();
     }
-    
+        
+    /**
+     * Outputs relevant message to the console depending on how the end game sequence has been reached.
+     * Also closes the scanner in the player object.
+     */
     private void endGameSequence() {
         if (isWinner == true) {
             if (!isPlayerTurn) {
                 System.out.println("Congratulations! You beat the highly intelligent computer!");
             }
+            
             else {
                 System.out.println("GAME OVER- you lost to the highly unintelligent computer. How impressive!");
             }
         }
+        
         else {
             System.out.println("It is a draw!");
         }
         player.closeScanner();
     }
     
+    /**
+     * Helper function used to initialise the remainingMoves ArrayList with numbers between 1-9.
+     * @return An ArrayList populated with integers 1-9.
+     */
     private ArrayList<Integer> populateRemainingMoves() {
         ArrayList<Integer> initialisedRemainingMoves = new ArrayList<Integer>();
         
