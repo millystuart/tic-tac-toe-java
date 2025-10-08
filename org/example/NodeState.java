d6bbc69d5dd78990a8bb136db284943526a6d25f
millystuart <melissaaa.stuart@gmail.com>
2025-10-08 09:47:07 +0100

    Define the NodeState enum as its own class rather than within Main

---
+++ org/example/NodeState.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * Holds all of the possible states a grid node can be in.
+ * These include X, O and EMPTY.
+ */
+public enum NodeState {
+    X,
+    O,
+    EMPTY
+}
